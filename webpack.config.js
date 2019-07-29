ef97acaa325fab700473b0fe4d12541e8f127907
付金亮 <738623786@qq.com>
2019-07-29 17:26:48 +0800

    创建第一个基于webpack的依赖库(library)

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+/**
+ * Created by fjl on 2019/7/29
+ */
+
+var path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: "webpack-numbers.js",
+		library: 'webpackNumbers',
+		libraryTarget: "umd"
+	},
+	externals: {
+		lodash: {
+			commonjs: 'lodash',
+			commonjs2: 'lodash',
+			amd: 'lodash',
+			root: '_'
+		}
+	},
+};
+
